8ebd83378532c580e4f55df978c26da728ea67a5
Satvik Bhat <157195484+thesat7vik@users.noreply.github.com>
2025-06-27 11:14:06 +0530

    nice

--- target/debug/deps/text_editor.d
+++ target/debug/deps/text_editor.d
@@ -1,6 +1,7 @@
-C:\Users\Satvik\Documents\Rust\text_editor\target\debug\deps\text_editor.exe: src/main.rs src\editor.rs
+C:\Users\Satvik\Documents\Rust\text_editor\target\debug\deps\text_editor.exe: src/main.rs src\editor.rs src\editor\terminal.rs
 
-C:\Users\Satvik\Documents\Rust\text_editor\target\debug\deps\text_editor.d: src/main.rs src\editor.rs
+C:\Users\Satvik\Documents\Rust\text_editor\target\debug\deps\text_editor.d: src/main.rs src\editor.rs src\editor\terminal.rs
 
 src/main.rs:
 src\editor.rs:
+src\editor\terminal.rs:
